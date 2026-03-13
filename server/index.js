c0afc01b95c5574feff7aa0c1e51829a9d58ad24
Toheeb Olufade <olufade.toheeb@gmail.com>
2026-03-13 13:34:58 +0000

    feat: add auth controller, custom error classes, error handling middleware and authentication middleware

--- server/index.js
+++ server/index.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./db/connect');
 
+const cors = require('cors');
+
+const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+
+const authRouter = require('./routes/auth');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 app.use(cors());
 
-connectDB();
+app.use('/api/v1/auth', authRouter);
 
-app.listen(5000,() => {
-    console.log('Server running on port 5000....');
-} );
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
