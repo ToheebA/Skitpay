b827a1cf6c2512054188134eb8152a8a41ff6809
Toheeb Olufade <olufade.toheeb@gmail.com>
2026-03-31 16:45:45 +0100

    fix: update cors origin url

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://skitpayfrontend.vercel.app'
+        'https://skitpay.vercel.app'
     ]
 }));
 
