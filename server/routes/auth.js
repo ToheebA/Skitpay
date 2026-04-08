06f05ebd410eb023cc16e25452c0482f2cf430f5
Toheeb Olufade <olufade.toheeb@gmail.com>
2026-04-08 13:46:39 +0100

    feat: add verify email route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, register } = require('../controllers/authController');
+const { login, register, verifyEmail } = require('../controllers/authController');
 
 router.post('/register', register);
+router.get('/verify-email', verifyEmail)
 router.post('/login', login);
 
 module.exports = router;
